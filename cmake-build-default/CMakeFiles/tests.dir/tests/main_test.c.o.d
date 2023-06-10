e29bc7cce1a44f86d3cea01b672c1b0324618ca9
Mateusz <mateusz@pop-os.localdomain>
2023-06-10 19:01:27 +0200

    update build

---
+++ cmake-build-default/CMakeFiles/tests.dir/tests/main_test.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/tests.dir/tests/main_test.c.o: \
+ /home/mateusz/.github/cpu-usage-tracker/tests/main_test.c \
+ /usr/include/stdc-predef.h
---
+++ cmake-build-default/CMakeFiles/tests.dir/tests/reader_test.c.o.d
@@ -0,0 +1,20 @@
+CMakeFiles/tests.dir/tests/reader_test.c.o: \
+ /home/mateusz/.github/cpu-usage-tracker/tests/reader_test.c \
+ /usr/include/stdc-predef.h \
+ /home/mateusz/.github/cpu-usage-tracker/tests/../reader.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h /usr/include/assert.h
